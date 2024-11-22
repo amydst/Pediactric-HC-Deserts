268f893cc157f645d9a3297c6e35bb4c918bcf80
amydst <amy.dst@gmail.com>
2024-11-22 12:47:29 -0800

    schema update 11.22

--- Healthcare_deserts_schema.sql
+++ Healthcare_deserts_schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE Population(
 	ZIP_Code varchar (5) NOT NULL,	
 	Population_Under_18_years int NOT NULL,	
 	Total_Population int NOT NULL,
-	Percent_Population_Under_18_years int,
+	Percent_Population_Under_18_years float,
 	Male_Pop_Under_18 int,	
 	Female_Pop_Under_18 int,
 	Population_Density_per_sq_mile float,
@@ -10,10 +10,8 @@ CREATE TABLE Population(
 ); 
 
 CREATE TABLE Demographics(
-	Demo_ID int NOT NULL,
+	Demo_ID float NOT NULL,
 	ZIP_Code varchar(5) NOT NULL,
-	Doctors_count float,
-	Kids_per_doctor float,
 	Household_Median_Income float,
 	Household_Mean_Income float,
 	Family_Median_Income float,
@@ -31,9 +29,10 @@ CREATE TABLE Demographics(
 	Per_Capita_Income float,
 	Insurance_coverage_status float,
 	Insurance_coverage_Rate float,
-	Doctor_Count int,
+	Doctor_Count float,
 	PRIMARY KEY (Demo_ID),
 	FOREIGN KEY (ZIP_Code) REFERENCES Population(ZIP_Code)
 );
+
 
 
